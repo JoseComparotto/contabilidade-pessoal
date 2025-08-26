688011e42ecff7225763181183cd58bd1fa8a2ee
José Comparotto <jose.comparotto@gmail.com>
2025-08-26 11:55:37 -0300

    Initialize Spring Boot project with basic structure and configuration files

---
+++ src/main/java/me/josecomparotto/contabilidade_pessoal/ContabilidadePessoalApplication.java
@@ -0,0 +1,13 @@
+package me.josecomparotto.contabilidade_pessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContabilidadePessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContabilidadePessoalApplication.class, args);
+	}
+
+}
